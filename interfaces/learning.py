46ef8f34b32bb22373aeffb1b318c92d22feed73
Chatavut Viriyasuthee <p.virie@gmail.com>
2026-01-30 12:06:17 +0700

    removed supervise learning instance

--- interfaces/learning.py
+++ interfaces/learning.py
@@ -14,10 +14,3 @@ class RL_Learner(abc.ABC):
               next_dones: List[List[bool]],
               valid_actions: Any = None, masks: Any = None, svl_masks: Any = None):
         pass
-
-
-class Supervised_Learner(abc.ABC):
-
-    @abc.abstractmethod
-    def train(self, obs: Any, actions: Any, target_actions: Any, valid_actions: Any = None, trained_logprob_indices: List[int] = None):
-        pass
