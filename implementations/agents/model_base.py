7c88ef4d0d22beed42b24b521180319ad2aa8a22
Chatavut Viriyasuthee <p.virie@gmail.com>
2026-02-21 16:15:49 +0700

    implement model 64

---
+++ implementations/agents/model_base.py
@@ -0,0 +1,259 @@
+import numpy as np
+from enum import Enum
+from interfaces.learning import RL_Learner
+from interfaces.agent import Agent
+from interfaces.network import Policy_Network, Value_Network
+from interfaces.data_structure import Context_Collector
+
+
+def make_valid_mask(valid_actions, action_size):
+    """
+    valid_actions: a list (batch) of list of int
+    action_size: int
+    """
+    valid_mask = np.zeros((len(valid_actions), action_size), dtype=np.bool)
+    for i, va in enumerate(valid_actions):
+        valid_mask[i, va] = True
+    return valid_mask
+
+
+def apply_cascading_masks(masks, *stop_conditions):
+    """
+    Updates masks by zeroing out elements where ANY stop_condition is True.
+    
+    Args:
+        masks (np.array): The initial mask array.
+        *stop_conditions (list): Variable number of lists/arrays to stack and filter by.
+                                 (e.g., last_idles, last_truncates)
+    """
+    combined_stop_flags = None
+
+    for cond in stop_conditions:
+        current_flags = np.stack(cond, axis=1).astype(bool)
+        if combined_stop_flags is None:
+            combined_stop_flags = current_flags
+        else:
+            # In-place logical_or is memory efficient and fast
+            np.logical_or(combined_stop_flags, current_flags, out=combined_stop_flags)
+
+    # ~combined_stop_flags turns True (stop) into False.
+    # .astype(float) turns False into 0.0, True into 1.0.
+    keep_factor = (~combined_stop_flags).astype(np.float32)
+    return masks * keep_factor
+
+
+class Model_Base(Agent):
+    
+    def __init__(self, 
+                 policy_model: Policy_Network, value_model: Value_Network,
+                 trainer: RL_Learner, 
+                 context_collector: Context_Collector, 
+                 action_collector: Context_Collector, 
+                 valid_action_collector: Context_Collector,
+                 max_num_thought_steps: int = 2,
+                 do_supervision: bool = False
+                 ):
+        self.policy_model = policy_model
+        self.value_model = value_model
+        self.trainer = trainer
+        self.obs = context_collector
+        self.actions = action_collector
+        self.valid_actions = valid_action_collector
+
+        self.max_num_thought_steps = max_num_thought_steps
+        self.do_supervision = do_supervision
+
+        self.valid_int_actions = list(range(self.policy_model.flag_size))
+        self.observe_external_int_actions = [0]
+
+        self.reset()
+
+
+    def reset(self):
+        self.obs.clear()
+        self.actions.clear()
+        self.valid_actions.clear()
+        self.rewards = []
+        self.last_idles = []
+        self.last_dones = []
+        self.last_truncates = []
+
+        self.thought_steps = None
+
+
+    def choose_action(self, 
+                      last_idles, last_dones, last_truncates, last_resets, 
+                      latest_frames, rewards, next_available_actions, 
+                      force_train=False):
+
+        batch_size = len(latest_frames)
+        current_cl = len(self.rewards)
+
+        # initialize
+        if self.thought_steps is None:
+            self.thought_steps = [0 for _ in range(batch_size)]
+            self.obs.append(
+                np.zeros((batch_size, self.policy_model.packed_context_size), dtype=np.float32)
+            )
+            self.actions.append(
+                np.zeros((batch_size, self.policy_model.packed_action_size), dtype=np.float32)
+            )
+            self.valid_actions.append(
+                np.ones((batch_size, self.policy_model.flag_size), dtype=np.bool),
+                np.ones((batch_size, self.policy_model.action_size), dtype=np.bool)
+            )
+            self.rewards.append(
+                np.zeros((batch_size,), dtype=np.float32)
+            )
+            self.last_idles.append([False for _ in range(batch_size)])
+            self.last_dones.append([False for _ in range(batch_size)])
+            self.last_truncates.append([True for _ in range(batch_size)])
+        
+        content = np.reshape(np.stack(latest_frames, axis=0), (batch_size, -1)) # content must be batch leading tensor (batch_size, ...)
+        reward = np.array([r for r in rewards])
+
+        # get last action's position
+        last_action = self.actions.get_last()
+        int_action, ext_action, position, last_content = self.policy_model.unpack_action(last_action)
+
+        for i, (idle, d, t, r) in enumerate(zip(last_idles, last_dones, last_truncates, last_resets)):
+            flag = int_action[i].item()
+            if idle:
+                # if idle, use content and reward from last thought
+                content[i, :] = last_content[i, :]
+                reward[i] = 0
+            if d or t:
+                self.thought_steps[i] = 0
+            self.last_idles[-1][i] = idle
+            self.last_dones[-1][i] = d
+            self.last_truncates[-1][i] = t
+
+        # replace the reward and content
+        self.obs.update_last(
+            self.policy_model.pack_context(
+                b_reward=reward,
+                b_int=int_action,
+                b_ext=ext_action,
+                b_position=position,
+                b_content=content
+            )
+        )
+        self.rewards[-1] = reward
+        self.valid_actions.update_last(
+            make_valid_mask([
+                self.observe_external_int_actions if self.thought_steps[i] == self.max_num_thought_steps else self.valid_int_actions 
+                for i in range(batch_size)
+            ], self.policy_model.flag_size),
+            make_valid_mask(next_available_actions, self.policy_model.action_size)
+        )
+
+        # if (any(last_dones) or any(last_truncates) or force_train) and current_cl > 1:
+        if force_train and current_cl > 1:
+            
+            if self.do_supervision:
+                # learn Supervise content
+                # masks has shape (batch_size, context_length)
+                svl_masks = apply_cascading_masks(
+                    self.actions.make_mask(batch_led=True)[:, :-1],
+                    self.last_idles[1:],
+                    self.last_dones[1:],
+                    self.last_truncates[1:]
+                )
+            else:
+                svl_masks = None
+
+            # learn RL
+
+            # masks has shape (batch_size, context_length)
+            # need to shift last_truncates by 1 to the left, because t signals whether t-1 is truncated
+            masks = apply_cascading_masks(
+                self.actions.make_mask(batch_led=True)[:, :-1], 
+                self.last_truncates[1:]
+            )
+
+            self.trainer.learn(
+                obs=self.obs.make_batch(batch_led=True), 
+                last_actions=self.actions.make_batch(batch_led=True), 
+                rewards=self.rewards[1:],
+                next_dones=self.last_dones[1:],
+                valid_actions=self.valid_actions[:-1].make_batch(batch_led=True),
+                masks=masks,
+                aux_masks=svl_masks
+            )
+            
+            left_over_slide = self.obs.mark(skip_last=True)
+            self.actions.mark(skip_last=True)
+            self.valid_actions.mark(skip_last=True)
+            self.rewards = self.rewards[left_over_slide]
+            self.last_idles = self.last_idles[left_over_slide]
+            self.last_dones = self.last_dones[left_over_slide]
+            self.last_truncates = self.last_truncates[left_over_slide]
+
+
+        # Choose a random action
+        packed_action = self.policy_model.get_action(
+            self.obs.get_last_batch(batch_led=True),
+            self.valid_actions.get_last_batch(batch_led=True)
+        )
+        packed_action = packed_action[:, -1, ...]
+
+        # extract output here
+        int_action, ext_action, position, content = self.policy_model.unpack_action(packed_action)
+
+        # check flag for external observation override and update thought steps
+        return_action = [None for _ in range(batch_size)]
+        for i in range(batch_size):
+            flag = int_action[i].item()
+            self.thought_steps[i] += 1
+            if flag in self.observe_external_int_actions:
+                return_action[i] = ext_action[i]
+                self.thought_steps[i] = 0
+
+        # populate last states
+        self.obs.append(
+            np.zeros((batch_size, self.policy_model.packed_context_size), dtype=np.float32)
+        )
+        self.actions.append(packed_action)
+        self.valid_actions.append(
+            np.ones((batch_size, self.policy_model.flag_size), dtype=np.bool),
+            np.ones((batch_size, self.policy_model.action_size), dtype=np.bool)
+        )
+        self.rewards.append(np.zeros((batch_size, 1), dtype=np.float32))
+        self.last_idles.append([return_action[i] is None for i in range(batch_size)])
+        self.last_dones.append([False for _ in range(batch_size)])
+        self.last_truncates.append([False for _ in range(batch_size)])
+
+        return return_action
+    
+
+if __name__ == "__main__":
+    # test mask
+
+    masks = np.ones((2, 5), dtype=np.float32)
+    last_idles = [
+        [False, True],
+        [False, False],
+        [True, False],
+        [False, False],
+        [False, True],
+    ]
+    last_dones = [
+        [False, False],
+        [False, True],
+        [False, False],
+        [True, True],
+        [False, False],
+    ] 
+    last_truncates = [
+        [False, False],
+        [True, False],
+        [False, False],
+        [True, False],
+        [False, False],
+    ]  
+    updated_masks = apply_cascading_masks(masks, last_idles, last_dones, last_truncates)
+    manual_masks = masks.copy()
+    manual_masks = manual_masks * (1.0 - np.stack(last_idles, axis=1).astype(np.float32))
+    manual_masks = manual_masks * (1.0 - np.stack(last_dones, axis=1).astype(np.float32))
+    manual_masks = manual_masks * (1.0 - np.stack(last_truncates, axis=1).astype(np.float32))
+    assert np.allclose(updated_masks, manual_masks), "Cascading mask application failed!"
---
+++ implementations/networks/torch/policy/dualism.py
@@ -0,0 +1,331 @@
+import torch
+import torch.nn as nn
+from torch.distributions.normal import Normal
+from torch.distributions.categorical import Categorical
+from torch.distributions import Bernoulli
+import numpy as np
+import logging
+
+from implementations.networks.torch.components.base import init_weights
+from implementations.networks.torch.components.base import Categorical_With_Mask
+from implementations.networks.torch.components.std_conv import ImpalaCNN
+from implementations.networks.torch.policy.base import Policy_Core as Base_Policy_Core, Projector as Base_Projector
+from utilities.safe_torch_module import Safe_nn_Module
+
+
+class Policy_Core(Base_Policy_Core):
+
+    def __init__(self, 
+                 mem_ops_size, action_size, position_size, 
+                 width, height, channel, 
+                 hidden_size, layers, 
+                 history_steps=0, max_temporal_len=32, 
+                 device=None, 
+                 persistence_path=None, first_load_path=None):
+        nn.Module.__init__(self)
+        Safe_nn_Module.__init__(self, name="dualism_core", device=device, persistence_path=persistence_path)
+        self.device = device
+
+        self.flag_size = mem_ops_size  # num classes for flag
+        self.action_size = action_size
+        self.position_size = position_size
+        self.content_size = channel * width * height
+        self.packed_action_size = 1 + 1 + position_size + self.content_size  # int_flag + action + x + y + position + content
+        self.packed_context_size = 1 + 1 + 1 + position_size + self.content_size  # reward + packed_action_size
+
+        self.width = width
+        self.height = height
+        self.channel = channel
+        self.hidden_size = hidden_size
+
+        self.conv_layers = ImpalaCNN(output_dims=hidden_size, input_channels=channel, width=width, height=height, depths=layers)
+        vec_dim = self.flag_size + action_size + position_size
+
+        self.position_step = nn.Sequential(
+            nn.Linear(vec_dim, hidden_size),
+            nn.GELU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.GELU(),
+            nn.Linear(hidden_size, position_size),
+        )
+
+        self.obs_feature_to_mean = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size),
+            nn.GELU(),
+            nn.Linear(hidden_size, position_size)
+        )
+
+        self.obs_feature_to_logvar = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size),
+            nn.GELU(),
+            nn.Linear(hidden_size, position_size)
+        )
+
+        self.head_flag = nn.Sequential(
+            nn.Linear(position_size, hidden_size),
+            nn.GELU(),
+            nn.Linear(hidden_size, self.flag_size)   # self.flag_size classes
+        )
+        self.head_action = nn.Sequential(
+            nn.Linear(position_size, hidden_size),
+            nn.GELU(),
+            nn.Linear(hidden_size, action_size)   # action_size classes
+        )
+
+        self.reset_parameters()
+        self.load(override_persistence_path=first_load_path)
+        self.eval()
+
+
+    def slow_parameters(self):
+        # override to return all parameters except the recognize_module parameters
+        params = []
+        for name, param in self.named_parameters():
+            if "recognize_module" not in name:
+                params.append(param)
+        return params
+
+
+    def fast_parameters(self):
+        # override to return only the recognize_module parameters
+        params = []
+        for name, param in self.named_parameters():
+            if "recognize_module" in name:
+                params.append(param)
+        return params
+
+
+    def reset_parameters(self):
+        # Reset parameters of all layers
+        self.conv_layers.reset_parameters()
+        self.position_step.apply(init_weights)
+
+        def init_actor_weights(m):
+            if isinstance(m, nn.Linear):
+                nn.init.orthogonal_(m.weight, gain=0.01)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                nn.init.orthogonal_(m.weight, gain=0.01)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+        self.obs_feature_to_mean.apply(init_actor_weights) # need near zero initialization for stable training with reparameterization trick
+        self.obs_feature_to_logvar.apply(init_actor_weights) # need near zero initialization for stable training with reparameterization trick
+        self.head_flag.apply(init_actor_weights)
+        self.head_action.apply(init_actor_weights)
+
+
+    def compute(self, context):
+        # context has shape (batch, context_size, self.packed_context_size)
+        batch_size = context.size(0)
+        context_size = context.size(1)
+
+        # first slice the image content
+        image_content = context[:, :, (1 + 1 + 1 + self.position_size): ]  # (batch_size, context_size, content_size)
+        image_part = torch.reshape(image_content, (batch_size, context_size, self.channel, self.height, self.width))
+        last_position = context[:, :, (1 + 1 + 1): (1 + 1 + 1 + self.position_size)]  # (batch_size, context_size, position_size)
+
+        # make one hot encoding for action, location
+        reward = context[:, :, 0:1]  # (batch_size, context_size, 1)
+        flag_onehot = torch.nn.functional.one_hot(context[:, :, 1].long(), num_classes=self.flag_size).float()
+        action_onehot = torch.nn.functional.one_hot(context[:, :, 2].long(), num_classes=self.action_size).float()
+        
+        vec = torch.concat([flag_onehot, action_onehot, last_position], dim=-1)  # (batch_size, context_size, flag_size + action_size + position_size)
+        step_position_logits = self.position_step(vec) + last_position  # (batch_size, context_size, position_size)
+
+        obs_logits = self.conv_layers(image_part.view(batch_size * context_size, self.channel, self.height, self.width))  # (batch_size * context_size, conv_output_size)
+
+        # use reparameterize trick to sample position
+        obs_means = self.obs_feature_to_mean(obs_logits)  # (batch_size * context_size, position_size)
+        obs_logvars = self.obs_feature_to_logvar(obs_logits)  # (batch_size * context_size, position_size)
+        std = torch.exp(0.5 * obs_logvars) # convert log-var to std
+        epsilon = torch.randn_like(std) # sample random noise
+        obs_position_logits = obs_means + epsilon * std
+
+        vae_loss = torch.sum(
+            torch.exp(obs_logvars) + obs_means**2 - 1.0 - obs_logvars,
+            dim=-1
+        )  # (B, T)
+
+        positions = step_position_logits + obs_position_logits # (batch_size, context_size, position_size)
+
+        logits_flag = self.head_flag(positions)    # (B, T, flag_size)
+        logits_action = self.head_action(positions) # (B, T, action_size)
+
+        return logits_flag, logits_action, positions, vae_loss
+    
+
+    def get_action(self, context, valid_actions=None):
+
+        if isinstance(context, np.ndarray):
+            context = torch.tensor(context, dtype=torch.float32).to(self.device)
+
+        batch_size = context.size(0)
+        context_size = context.size(1)
+
+        available_flags = None
+        available_actions = None
+        if valid_actions is not None:
+            if isinstance(valid_actions, np.ndarray):
+                valid_actions = torch.tensor(valid_actions, dtype=torch.bool).to(self.device)
+            # valid_actions has shape (batch, context_size, flag_size + action_size)
+            available_flags = valid_actions[:, :, :self.flag_size].to(self.device)
+            available_actions = valid_actions[:, :, self.flag_size:].to(self.device)
+
+        logits_flag, logits_action, positions, _ = self.compute(context)
+
+        probs_flag = Categorical_With_Mask(logits=logits_flag, mask=available_flags)
+        probs_action = Categorical_With_Mask(logits=logits_action, mask=available_actions)
+
+        action_flag = probs_flag.sample()
+        action_action = probs_action.sample()
+        action_content = np.zeros((batch_size, context_size, self.content_size), dtype=np.float32)
+
+        action = torch.cat([
+            action_flag.unsqueeze(-1),
+            action_action.unsqueeze(-1),
+            positions.detach(),
+            action_content
+        ], dim=-1)
+
+        return action.cpu().numpy().astype(float)
+    
+
+    def get_log_probability(self, context, selected_action, valid_actions=None):
+        # context has shape (batch, context_size, self.packed_context_size)
+
+        if isinstance(context, np.ndarray):
+            context = torch.tensor(context, dtype=torch.float32).to(self.device)
+
+        available_flags = None
+        available_actions = None
+        if valid_actions is not None:
+            if isinstance(valid_actions, np.ndarray):
+                valid_actions = torch.tensor(valid_actions, dtype=torch.bool).to(self.device)
+            # valid_actions has shape (batch, context_size, flag_size + action_size)
+            available_flags = valid_actions[:, :, :self.flag_size].to(self.device)
+            available_actions = valid_actions[:, :, self.flag_size:].to(self.device)
+
+        logits_flag, logits_action, positions, _ = self.compute(context)
+
+        probs_flag = Categorical_With_Mask(logits=logits_flag, mask=available_flags)
+        probs_action = Categorical_With_Mask(logits=logits_action, mask=available_actions)
+
+        action_flag = selected_action[:, :, 0]
+        action_action = selected_action[:, :, 1]
+
+        log_prob_flag = probs_flag.log_prob(action_flag)
+        log_prob_action = probs_action.log_prob(action_action)
+        
+        entropy_flag = probs_flag.entropy()
+        entropy_action = probs_action.entropy()
+
+        return torch.stack([
+            log_prob_flag, log_prob_action
+        ], dim=-1), torch.stack([
+            entropy_flag, entropy_action
+        ], dim=-1)
+
+
+    def get_log_probability_with_aux_loss(self, context, selected_action, valid_actions=None):
+        # now context has shape (batch, context_size + 1, self.packed_context_size)
+
+        if isinstance(context, np.ndarray):
+            context = torch.tensor(context, dtype=torch.float32).to(self.device)
+
+        context_full = context
+        context = context_full[:, :-1, :]  # remove last context for computing logprob
+        # target_position = context_full[:, 1:, (1 + 1 + 1):(1 + 1 + 1 + self.position_size)]  # only position part for position loss
+
+        available_flags = None
+        available_actions = None
+        if valid_actions is not None:
+            if isinstance(valid_actions, np.ndarray):
+                valid_actions = torch.tensor(valid_actions, dtype=torch.bool).to(self.device)
+            # valid_actions has shape (batch, context_size, flag_size + action_size)
+            available_flags = valid_actions[:, :, :self.flag_size].to(self.device)
+            available_actions = valid_actions[:, :, self.flag_size:].to(self.device)
+
+        logits_flag, logits_action, positions, aux_loss = self.compute(context)
+
+        probs_flag = Categorical_With_Mask(logits=logits_flag, mask=available_flags)
+        probs_action = Categorical_With_Mask(logits=logits_action, mask=available_actions)
+
+        action_flag = selected_action[:, :, 0]
+        action_action = selected_action[:, :, 1]
+
+        log_prob_flag = probs_flag.log_prob(action_flag)
+        log_prob_action = probs_action.log_prob(action_action)
+        
+        entropy_flag = probs_flag.entropy()
+        entropy_action = probs_action.entropy()
+
+        return torch.stack([
+            log_prob_flag, log_prob_action
+        ], dim=-1), torch.stack([
+            entropy_flag, entropy_action
+        ], dim=-1), aux_loss
+
+
+    def unpack_action(self, packed_action):
+        # packed_action has shape (batch, self.packed_action_size)
+        # return int_action, ext_action, position, content
+
+        int_part = packed_action[:, 0].astype(int)
+        ext_part = packed_action[:, 1:2].astype(int)
+        position = packed_action[:, 2:2 + self.position_size].astype(float)
+        content = packed_action[:, 2 + self.position_size:].astype(float)
+
+        return int_part, ext_part, position, content
+    
+
+    def pack_context(self, b_reward=None, b_int=None, b_ext=None, b_position=None, b_content=None):
+        # b_xxx has shape (batch, ...)
+        # return packed_action_seq of shape (batch, self.packed_action_size) of type int
+        # replace none with zeros
+
+        batch_size = None
+        if b_reward is not None:
+            batch_size = b_reward.shape[0]
+        elif b_int is not None:
+            batch_size = b_int.shape[0]
+        elif b_ext is not None:
+            batch_size = b_ext.shape[0]
+        elif b_position is not None:
+            batch_size = b_position.shape[0]
+        elif b_content is not None:
+            batch_size = b_content.shape[0]
+        else:
+            raise ValueError("At least one of b_reward, b_content must be provided")
+        
+        if b_reward is None:
+            b_reward = np.zeros((batch_size,), dtype=float)
+        if b_int is None:
+            b_int = np.zeros((batch_size,), dtype=int)
+        if b_ext is None:
+            b_ext = np.zeros((batch_size, 1), dtype=int)
+        if b_position is None:
+            b_position = np.zeros((batch_size, self.position_size), dtype=float)
+        if b_content is None:
+            b_content = np.zeros((batch_size, self.content_size), dtype=float)
+
+        packed_context = np.concatenate([
+            np.reshape(b_reward, (batch_size, 1)),
+            np.reshape(b_int, (batch_size, 1)),
+            b_ext,
+            b_position,
+            b_content
+        ], axis=-1).astype(float)
+
+        return packed_context
+
+
+# return only selected statistics
+class Projector(Base_Projector):
+
+    def slow_parameters(self):
+        return self.master_core.slow_parameters()
+    
+    def fast_parameters(self):
+        return self.master_core.fast_parameters()
