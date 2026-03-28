f47b1a0c3cb483088b7ae841830db4d22b430b67
Chatavut Viriyasuthee <p.virie@gmail.com>
2026-03-28 16:34:01 +0700

    add cultivating hierarchy

--- implementations/networks/torch/policy/cultivate_token.py
+++ implementations/networks/torch/policy/cultivate_token.py
@@ -9,7 +9,7 @@ import logging
 from implementations.networks.torch.components.base import init_weights
 from implementations.networks.torch.components.base import Categorical_With_Mask
 from implementations.networks.torch.components.std_resnet import ResNet
-from implementations.networks.torch.policy.base_xy import Policy_Core as Base_Policy_Core
+from implementations.networks.torch.policy.base_token import Policy_Core as Base_Policy_Core
 from utilities.safe_torch_module import Safe_nn_Module
 
 
@@ -17,9 +17,10 @@ class Policy_Core(Base_Policy_Core):
 
     def __init__(self, 
                  int_action_size, ext_action_size, 
-                 position_size, content_size,
+                 obs_size, goal_size,
                  dict_size, embedding_dim,
                  hidden_size, layers, 
+                 history_steps=0, max_temporal_len=32, 
                  device=None, 
                  persistence_path=None, first_load_path=None):
         nn.Module.__init__(self)
@@ -28,18 +29,25 @@ class Policy_Core(Base_Policy_Core):
 
         self.int_action_size = int_action_size  # num classes for flag
         self.ext_action_size = ext_action_size
-        self.position_size = position_size
-        self.content_size = content_size
-        self.packed_action_size = 1 + 1 + position_size + self.content_size  # int_action_size + ext_action_size + position + content
-        self.packed_context_size = 1 + 1 + 1 + position_size + self.content_size  # reward + packed_action_size
+        self.position_size = goal_size # use position to store sub-goal information
+        self.content_size = obs_size + goal_size
+        self.packed_action_size = 1 + 1 + goal_size + obs_size + goal_size
+        self.packed_context_size = 1 + 1 + 1 + goal_size + obs_size + goal_size
 
+        self.goal_size = goal_size
+        self.obs_size = obs_size
         self.hidden_size = hidden_size
 
-        self.embedding = nn.Embedding(dict_size, embedding_dim)  # for tokens
-        
-        vec_dim = self.int_action_size + ext_action_size + position_size + content_size * embedding_dim
-        self.backbone = ResNet(output_dims=hidden_size, input_dims=vec_dim, hidden_dims=hidden_size, layers=layers)
+        self.goal_embedding = nn.Embedding(dict_size, embedding_dim)  # for goal token
+        self.obs_embedding = nn.Embedding(dict_size, embedding_dim)  # for obs token
 
+        predict_input_dim = (goal_size + obs_size) * embedding_dim # goal, obs -> hidden
+        self.backbone = ResNet(output_dims=hidden_size, input_dims=predict_input_dim, hidden_dims=hidden_size, layers=layers)
+
+        evaluate_input_dim = (obs_size) * embedding_dim
+        self.evaluator = ResNet(output_dims=obs_size * embedding_dim, input_dims=evaluate_input_dim, hidden_dims=hidden_size, layers=layers)
+
+        # hidden -> action
         self.head_int = nn.Sequential(
             nn.Linear(hidden_size, hidden_size),
             nn.GELU(),
@@ -50,10 +58,12 @@ class Policy_Core(Base_Policy_Core):
             nn.GELU(),
             nn.Linear(hidden_size, ext_action_size)   # ext_action_size classes
         )
-        self.head_position = nn.Sequential(
+
+        # hidden -> sub goal
+        self.head_subgoal = nn.Sequential(
             nn.Linear(hidden_size, hidden_size),
             nn.GELU(),
-            nn.Linear(hidden_size, position_size)
+            nn.Linear(hidden_size, goal_size * embedding_dim)   # predict next subgoal in embedded space
         )
 
         self.reset_parameters()
@@ -63,8 +73,12 @@ class Policy_Core(Base_Policy_Core):
 
     def reset_parameters(self):
         # Reset parameters of all layers
-        self.embedding.reset_parameters()
+
+        self.goal_embedding.reset_parameters()
+        self.obs_embedding.reset_parameters()
+
         self.backbone.reset_parameters()
+        self.evaluator.reset_parameters()
 
         def init_actor_weights(m):
             if isinstance(m, nn.Linear):
@@ -78,7 +92,7 @@ class Policy_Core(Base_Policy_Core):
 
         self.head_int.apply(init_actor_weights)
         self.head_ext.apply(init_actor_weights)
-        self.head_position.apply(init_actor_weights)
+        self.head_subgoal.apply(init_actor_weights)
 
 
     def compute(self, context):
@@ -89,20 +103,44 @@ class Policy_Core(Base_Policy_Core):
         reward = context[:, :, 0:1]  # (batch_size, context_size, 1)
         flag_onehot = torch.nn.functional.one_hot(context[:, :, 1].long(), num_classes=self.int_action_size).float()
         action_onehot = torch.nn.functional.one_hot(context[:, :, 2].long(), num_classes=self.ext_action_size).float()
-        last_position = context[:, :, (1 + 1 + 1): (1 + 1 + 1 + self.position_size)]  # (batch_size, context_size, position_size)
-        content = context[:, :, (1 + 1 + 1 + self.position_size): ]  # (batch_size, context_size, content_size)
+        last_subgoal = context[:, :, (1 + 1 + 1): (1 + 1 + 1 + self.position_size)]  # (batch_size, context_size, goal_size)
+        obs = context[:, :, (1 + 1 + 1 + self.position_size):(1 + 1 + 1 + self.position_size + self.obs_size) ]  # (batch_size, context_size, obs_size)
+        goal = context[:, :, (1 + 1 + 1 + self.position_size + self.obs_size): ]  # (batch_size, context_size, goal_size)
 
-        embedded = self.embedding(content.long())  # (batch_size, context_size, content_size, embedding_dim)
-        embedded = embedded.view(batch_size, context_size, -1)  # (batch_size, context_size, content_size * embedding_dim)
+        last_subgoal_embedded = self.goal_embedding(last_subgoal.long())  # (batch_size, context_size, goal_size, embedding_dim)
+        last_subgoal_embedded = last_subgoal_embedded.view(batch_size, context_size, -1)  # (batch_size, context_size, goal_size * embedding_dim)
+        obs_embedded = self.obs_embedding(obs.long())  # (batch_size, context_size, obs_size, embedding_dim)
+        obs_embedded = obs_embedded.view(batch_size, context_size, -1)  # (batch_size, context_size, obs_size * embedding_dim)
+        goal_embedded = self.goal_embedding(goal.long())  # (batch_size, context_size, goal_size, embedding_dim)
+        goal_embedded = goal_embedded.view(batch_size, context_size, -1)  # (batch_size, context_size, goal_size * embedding_dim)
 
-        vec = torch.concat([flag_onehot, action_onehot, last_position, embedded], dim=-1)  # (batch_size, context_size, int_action_size + ext_action_size + position_size + content_size * embedding_dim)
-        backbone_output = self.backbone(vec)  # (batch_size, context_size, hidden_size)
+        base_input = torch.concat([goal_embedded, obs_embedded], dim=-1)  # (batch_size, context_size, (goal_size + obs_size) * embedding_dim)
+        base_output = self.backbone(base_input)  # (batch_size, context_size, hidden_size)
+
+        lwlv_input = torch.concat([last_subgoal_embedded, obs_embedded], dim=-1)  # (batch_size, context_size, (goal_size + obs_size) * embedding_dim)
+        lwlv_output = self.backbone(lwlv_input)  # (batch_size, context_size, hidden_size)
+
+        lw_eval_input = obs_embedded  # (batch_size, context_size, obs_size * embedding_dim)
+        lw_result = self.evaluator(lw_eval_input)  # (batch_size, context_size, obs_size * embedding_dim)
+
+        sub_input = torch.concat([goal_embedded, lw_result], dim=-1)  # (batch_size, context_size, (goal_size + obs_size) * embedding_dim)
+        sub_output = self.backbone(sub_input)  # (batch_size, context_size, hidden_size)
+
+        int_logits = self.head_int(base_output)  # (batch_size, context_size, int_action_size)
+        ext_logits = self.head_ext(base_output)  # (batch_size, context_size, ext_action_size)
         
-        int_logits = self.head_int(backbone_output)  # (batch_size, context_size, int_action_size)
-        ext_logits = self.head_ext(backbone_output)  # (batch_size, context_size, ext_action_size)
-        position_logits = self.head_position(backbone_output)  # (batch_size, context_size, position_size)
+        subgoal_logits = self.head_subgoal(sub_output)  # (batch_size, context_size, goal_size * embedding_dim)
 
-        return int_logits, ext_logits, position_logits
+        aux_int_logits = self.head_int(lwlv_output)  # (batch_size, context_size, int_action_size)
+        aux_ext_logits = self.head_ext(lwlv_output)  # (batch_size, context_size, ext_action_size)
+
+        # calculate aux loss
+        action_loss = torch.mean((int_logits - aux_int_logits)**2, dim=-1) + torch.mean((ext_logits - aux_ext_logits)**2, dim=-1)
+        # subgoal_logits should not deviate much from last_subgoal_embedded 
+        subgoal_loss = torch.mean((subgoal_logits - last_subgoal_embedded)**2, dim=-1)
+        aux_loss = action_loss + 0.1 * subgoal_loss
+
+        return int_logits, ext_logits, subgoal_logits, aux_loss
     
 
     def get_action(self, context, valid_actions=None):
@@ -119,18 +157,17 @@ class Policy_Core(Base_Policy_Core):
             available_flags = valid_actions[:, :, :self.int_action_size].to(self.device)
             available_actions = valid_actions[:, :, self.int_action_size:].to(self.device)
 
-        logits_int, logits_ext, position_logits = self.compute(context)
+        logits_int, logits_ext, position_logits, aux_loss = self.compute(context)
 
         probs_int = Categorical_With_Mask(logits=logits_int, mask=available_flags)
         probs_ext = Categorical_With_Mask(logits=logits_ext, mask=available_actions)
-        probs_position = Bernoulli(logits=position_logits)
 
         batch_size = context.size(0)
         context_size = context.size(1)
 
         action_int = probs_int.sample()
         action_ext = probs_ext.sample()
-        next_position = probs_position.sample()
+        next_position = torch.argmax(position_logits.view(batch_size, context_size, self.goal_size, -1), dim=-1)  # (batch_size, context_size, goal_size)
         action_content = np.zeros((batch_size, context_size, self.content_size), dtype=np.float32)
 
         action = np.concatenate([
@@ -158,30 +195,26 @@ class Policy_Core(Base_Policy_Core):
             available_flags = valid_actions[:, :, :self.int_action_size].to(self.device)
             available_actions = valid_actions[:, :, self.int_action_size:].to(self.device)
 
-        logits_int, logits_ext, position_logits = self.compute(context)
+        logits_int, logits_ext, position_logits, aux_loss = self.compute(context)
 
         probs_int = Categorical_With_Mask(logits=logits_int, mask=available_flags)
         probs_ext = Categorical_With_Mask(logits=logits_ext, mask=available_actions)
-        probs_position = Bernoulli(logits=position_logits)
 
         action_int = selected_action[:, :, 0]
         action_ext = selected_action[:, :, 1]
-        action_position = selected_action[:, :, (1 + 1): (1 + 1 + self.position_size)]
 
         log_prob_int = probs_int.log_prob(action_int)
         log_prob_ext = probs_ext.log_prob(action_ext)
-        log_prob_position = probs_position.log_prob(action_position).mean(-1)
         
         entropy_int = probs_int.entropy()
         entropy_ext = probs_ext.entropy()
-        entropy_position = probs_position.entropy().mean(-1)
 
         return torch.stack([
-            log_prob_int, log_prob_ext, log_prob_position
+            log_prob_int, log_prob_ext
         ], dim=-1), torch.stack([
-            entropy_int, entropy_ext, entropy_position
+            entropy_int, entropy_ext
         ], dim=-1)
-
+    
 
     def get_log_probability_with_aux_loss(self, context, selected_action, valid_actions=None):
         # now context has shape (batch, context_size + 1, self.packed_context_size)
@@ -201,80 +234,25 @@ class Policy_Core(Base_Policy_Core):
             available_flags = valid_actions[:, :, :self.int_action_size].to(self.device)
             available_actions = valid_actions[:, :, self.int_action_size:].to(self.device)
 
-        logits_int, logits_ext, position_logits = self.compute(context)
+        logits_int, logits_ext, position_logits, aux_loss = self.compute(context)
 
         probs_int = Categorical_With_Mask(logits=logits_int, mask=available_flags)
         probs_ext = Categorical_With_Mask(logits=logits_ext, mask=available_actions)
-        probs_position = Bernoulli(logits=position_logits)
 
         action_int = selected_action[:, :, 0]
         action_ext = selected_action[:, :, 1]
-        action_position = selected_action[:, :, (1 + 1): (1 + 1 + self.position_size)]
 
         log_prob_int = probs_int.log_prob(action_int)
         log_prob_ext = probs_ext.log_prob(action_ext)
-        log_prob_position = probs_position.log_prob(action_position).mean(-1)
         
         entropy_int = probs_int.entropy()
         entropy_ext = probs_ext.entropy()
-        entropy_position = probs_position.entropy().mean(-1)
 
         return torch.stack([
-            log_prob_int, log_prob_ext, log_prob_position
+            log_prob_int, log_prob_ext
         ], dim=-1), torch.stack([
-            entropy_int, entropy_ext, entropy_position
-        ], dim=-1), None  # no aux loss for now
+            entropy_int, entropy_ext
+        ], dim=-1), aux_loss  # no aux loss for now
 
 
-    def unpack_action(self, packed_action):
-        # packed_action has shape (batch, self.packed_action_size)
-        # return int_action, ext_action, position
-
-        int_part = packed_action[:, 0].astype(int)
-        ext_part = packed_action[:, 1:2].astype(int)
-        position = packed_action[:, 2:2 + self.position_size].astype(float)
-        content = packed_action[:, 2 + self.position_size:].astype(float)
-
-        return int_part, ext_part, position, content
-    
-
-    def pack_context(self, b_reward=None, b_int=None, b_ext=None, b_position=None, b_content=None):
-        # b_xxx has shape (batch, ...)
-        # return packed_action_seq of shape (batch, self.packed_action_size) of type int
-        # replace none with zeros
-
-        batch_size = None
-        if b_reward is not None:
-            batch_size = b_reward.shape[0]
-        elif b_int is not None:
-            batch_size = b_int.shape[0]
-        elif b_ext is not None:
-            batch_size = b_ext.shape[0]
-        elif b_position is not None:
-            batch_size = b_position.shape[0]
-        elif b_content is not None:
-            batch_size = b_content.shape[0]
-        else:
-            raise ValueError("At least one of b_reward, b_content must be provided")
-        
-        if b_reward is None:
-            b_reward = np.zeros((batch_size,), dtype=float)
-        if b_int is None:
-            b_int = np.zeros((batch_size,), dtype=int)
-        if b_ext is None:
-            b_ext = np.zeros((batch_size, 1), dtype=int)
-        if b_position is None:
-            b_position = np.zeros((batch_size, self.position_size), dtype=float)
-        if b_content is None:
-            b_content = np.zeros((batch_size, self.content_size), dtype=float)
-
-        packed_context = np.concatenate([
-            np.reshape(b_reward, (batch_size, 1)),
-            np.reshape(b_int, (batch_size, 1)),
-            b_ext,
-            b_position,
-            b_content
-        ], axis=-1).astype(float)
-
-        return packed_context
 
